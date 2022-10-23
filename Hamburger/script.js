b795ebd73808e85bfb5ca21a9c096415bdb25427
Vivek Singh <rogerthatvivek@gmail.com>
2022-10-24 04:22:51 +0530

    Hamburger Menu

---
+++ Hamburger/script.js
@@ -0,0 +1,4 @@
+document.getElementById('btn').addEventListener('click', ()=>{
+    document.getElementById('nav').classList.toggle('active')
+    document.getElementById('btn').classList.toggle('active')
+})
