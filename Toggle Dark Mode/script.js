1ac4aa4b223debc8ab5be2af65eb9060f8547081
Vivek Singh <rogerthatvivek@gmail.com>
2022-10-24 05:39:16 +0530

    Added Toggle Background

---
+++ Toggle Dark Mode/script.js
@@ -0,0 +1,7 @@
+document.getElementById('btn').addEventListener('click', ()=>{
+    document.body.style.backgroundColor=randomBg()
+})
+
+function randomBg(){
+    return `hsl(${Math.floor(Math.random()*360)}, 100%, 50%)`
+}
